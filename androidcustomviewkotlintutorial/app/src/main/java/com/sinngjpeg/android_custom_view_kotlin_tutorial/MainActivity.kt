0320cfa29c27f8c1194e555c1ec82f2e675cffdc
Ingrid Silva <32372431+sinngjpeg@users.noreply.github.com>
2022-07-25 11:39:00 -0300

    cria projeto

---
+++ androidcustomviewkotlintutorial/app/src/main/java/com/sinngjpeg/android_custom_view_kotlin_tutorial/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sinngjpeg.android_custom_view_kotlin_tutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
